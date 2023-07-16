144c16d0e48d4c8735225a8540f59c79f7e8bf72
zongjie233 <840573195@qq.com>
2023-07-16 22:26:09 +0800

    gohtml

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/zongjie233/lenslocked
 
 go 1.19
+
+require github.com/go-chi/chi/v5 v5.0.10 // indirect
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -13,32 +15,14 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact page</h1>")
 }
 
-// func pathHandler(w http.ResponseWriter, r *http.Request) {
-// 	switch r.URL.Path {
-// 	case "/":
-// 		homeHandler(w, r)
-// 	case "/contact":
-// 		contactHandler(w, r)
-// 	default:
-// 		http.Redirect(w, r, "/", http.StatusNotFound)
-// 	}
-// }
-
-type Router struct{}
-
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	default:
-		http.Error(w, "page not foune", http.StatusNotFound)
-	}
-}
-
 func main() {
-	var router Router
+	// var router Router
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "page not foune", http.StatusNotFound)
+	})
 	fmt.Println("starting the server on :3000....")
-	http.ListenAndServe(":3000", router)
+	http.ListenAndServe(":3000", r)
 }

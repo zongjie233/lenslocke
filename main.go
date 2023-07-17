0d042e1b91bbd577db48de1235a2005d4d4d63ee
zongjie233 <840573195@qq.com>
2023-07-17 21:19:44 +0800

    MVC模式

--- main.go
+++ main.go
@@ -2,47 +2,35 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/zongjie233/lenslocked/controllers"
 	"github.com/zongjie233/lenslocked/views"
 )
 
-func excuteTemplate(w http.ResponseWriter, filepath string) {
-	t, err := views.Parse(filepath)
-	if err != nil {
-		log.Printf("patsing template : %v", err)
-		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
-		return
-	}
-	t.Excute(w, nil)
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-
-	tplpath := filepath.Join("templates", "home.gohtml")
-	excuteTemplate(w, tplpath)
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-
-	tplpath := filepath.Join("templates", "contact.gohtml")
-	excuteTemplate(w, tplpath)
-}
-
-func faqHandler(w http.ResponseWriter, r *http.Request) {
-
-	excuteTemplate(w, filepath.Join("templates", "faq.gohtml"))
-}
-
 func main() {
-	// var router Router
 	r := chi.NewRouter()
-	r.Get("/", homeHandler)
-	r.Get("/contact", contactHandler)
-	r.Get("/faq", faqHandler)
+
+	tpl, err := views.Parse(filepath.Join("templates", "home.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/", controllers.StaticHandler(tpl))
+
+	tpl, err = views.Parse(filepath.Join("templates", "contact.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/contact", controllers.StaticHandler(tpl))
+
+	tpl, err = views.Parse(filepath.Join("templates", "faq.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/faq", controllers.StaticHandler(tpl))
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "page not foune", http.StatusNotFound)
 	})
--- views/template.go
+++ views/template.go
@@ -21,13 +21,13 @@ type Template struct {
 	htmlTpl *template.Template
 }
 
-func (t Template) Excute(w http.ResponseWriter, data interface{}) {
+func (t Template) Execute(w http.ResponseWriter, data interface{}) {
 	w.Header().Set("Content-type", "text/html,charset=utf-8")
 	err := t.htmlTpl.Execute(w, data)
 	if err != nil {
 		log.Printf("解析模板出错: %v", err)
 		http.Error(w, "there was an error parsing the template", http.StatusInternalServerError)
-		return // 不在运行下面的代码
+		return
 	}
 
 }

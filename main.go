36c2509fc69ebd056ad9eb56257f7b7d79413708
zongjie233 <840573195@qq.com>
2023-07-18 15:42:05 +0800

    embed template usage

--- main.go
+++ main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/zongjie233/lenslocked/controllers"
+	"github.com/zongjie233/lenslocked/templates"
 	"github.com/zongjie233/lenslocked/views"
 )
 
@@ -14,13 +14,13 @@ func main() {
 	r := chi.NewRouter()
 
 	r.Get("/", controllers.StaticHandler(
-		views.Must(views.Parse(filepath.Join("templates", "home.gohtml")))))
+		views.Must(views.ParseFS(templates.FS, "home.gohtml"))))
 
 	r.Get("/contact", controllers.StaticHandler(
-		views.Must(views.Parse(filepath.Join("templates", "contact.gohtml")))))
+		views.Must(views.ParseFS(templates.FS, "contact.gohtml"))))
 
 	r.Get("/faq", controllers.StaticHandler(
-		views.Must(views.Parse(filepath.Join("templates", "faq.gohtml")))))
+		views.Must(views.ParseFS(templates.FS, "faq.gohtml"))))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "page not foune", http.StatusNotFound)
--- views/template.go
+++ views/template.go
@@ -3,6 +3,8 @@ package views
 import (
 	"fmt"
 	"html/template"
+	"io/fs"
+
 	"log"
 	"net/http"
 )
@@ -12,6 +14,16 @@ func Must(t Template, err error) Template {
 		panic(err)
 	}
 	return t
+}
+
+func ParseFS(fs fs.FS, patterns ...string) (Template, error) {
+	tpl, err := template.ParseFS(fs, patterns...)
+	if err != nil {
+		return Template{}, fmt.Errorf("parsfs template: %v", err)
+	}
+	return Template{
+		htmlTpl: tpl,
+	}, nil
 }
 
 func Parse(filepath string) (Template, error) {

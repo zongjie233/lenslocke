2c9d12335a5567cefc997de01e90c2e2f86e6862
zongjie233 <840573195@qq.com>
2023-07-30 18:16:30 +0800

    feature signout

--- models/session.go
+++ models/session.go
@@ -39,11 +39,68 @@ func (ss *SessionService) Create(userID int) (*Session, error) {
 		Token:     token,
 		TokenHash: ss.hash(token),
 	}
+	//更新用户session,如果报错则创建session
+	row := ss.DB.QueryRow(`UPDATE
+		sessions 
+	SET
+		token_hash = $2 
+	WHERE
+		user_id = $1 RETURNING id;`, session.UserID, session.TokenHash)
+	err = row.Scan(&session.ID)
+	// 存储session
+	if err == sql.ErrNoRows {
+		row = ss.DB.QueryRow(`
+		INSERT INTO sessions (user_id,token_hash)
+		VALUES ($1,$2)
+		RETURNING id;`, session.UserID, session.TokenHash)
+		err = row.Scan(&session.ID)
+	}
+	if err != nil {
+		return nil, fmt.Errorf("create:%w", err)
+	}
 	return &session, nil
 }
 
 func (ss *SessionService) User(token string) (*User, error) {
-	return nil, nil
+	tokenHash := ss.hash(token)
+	var user User
+	row := ss.DB.QueryRow(`
+			SELECT
+				user_id 
+			FROM
+				sessions 
+			WHERE
+				token_hash = $1;`, tokenHash)
+	err := row.Scan(&user.ID)
+	if err != nil {
+		return nil, fmt.Errorf("user: %w", err)
+	}
+
+	row = ss.DB.QueryRow(`
+		SELECT
+			email,
+			password_hash 
+		FROM
+			users 
+		WHERE
+			id = $1;`, user.ID)
+	err = row.Scan(&user.Email, &user.PasswordHash)
+	if err != nil {
+		return nil, fmt.Errorf("user:%w", err)
+	}
+
+	return &user, nil
+}
+
+func (ss *SessionService) Delete(token string) error {
+	tokenHash := ss.hash(token)
+	_, err := ss.DB.Exec(`
+	DELETE FROM sessions 
+	WHERE token_hash = $1;`, tokenHash)
+	if err != nil {
+		return fmt.Errorf("delete: %w", err)
+	}
+	return nil
 }
 
 func (ss *SessionService) hash(token string) string {

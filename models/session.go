fd116113687425c87f4685d83dcf23a7718013b4
zongjie233 <840573195@qq.com>
2023-08-04 16:03:29 +0800

    change update sessions sql

--- models/session.go
+++ models/session.go
@@ -39,22 +39,20 @@ func (ss *SessionService) Create(userID int) (*Session, error) {
 		Token:     token,
 		TokenHash: ss.hash(token),
 	}
-	//更新用户session,如果报错则创建session
-	row := ss.DB.QueryRow(`UPDATE
-		sessions 
-	SET
-		token_hash = $2 
-	WHERE
-		user_id = $1 RETURNING id;`, session.UserID, session.TokenHash)
+	//更新用户session, 如果user_id部分存在冲突则进行更新,将token_hash设置为传入的新值,确保用户永远拥有一个最新的session
+	row := ss.DB.QueryRow(`
+			Insert     
+			Into
+				sessions
+				(user_id,token_hash)     
+			VALUES
+				($1,$2)     
+					ON CONFLICT (user_id) DO UPDATE
+						
+				SET
+					token_hash = $2 RETURNING id`, session.UserID, session.TokenHash)
 	err = row.Scan(&session.ID)
 	// 存储session
-	if err == sql.ErrNoRows {
-		row = ss.DB.QueryRow(`
-		INSERT INTO sessions (user_id,token_hash)
-		VALUES ($1,$2)
-		RETURNING id;`, session.UserID, session.TokenHash)
-		err = row.Scan(&session.ID)
-	}
 	if err != nil {
 		return nil, fmt.Errorf("create:%w", err)
 	}
@@ -65,30 +63,21 @@ func (ss *SessionService) User(token string) (*User, error) {
 	tokenHash := ss.hash(token)
 	var user User
 	row := ss.DB.QueryRow(`
-			SELECT
-				user_id 
-			FROM
-				sessions 
-			WHERE
-				token_hash = $1;`, tokenHash)
-	err := row.Scan(&user.ID)
+				SELECT
+			users.id,
+			users.email,
+			users.password_hash 
+		FROM
+			sessions 
+		JOIN
+			users 
+				ON users.id= sessions.user_id 
+		WHERE
+			sessions.token_hash = $1`, tokenHash)
+	err := row.Scan(&user.ID, &user.Email, &user.PasswordHash)
 	if err != nil {
 		return nil, fmt.Errorf("user: %w", err)
 	}
-
-	row = ss.DB.QueryRow(`
-		SELECT
-			email,
-			password_hash 
-		FROM
-			users 
-		WHERE
-			id = $1;`, user.ID)
-	err = row.Scan(&user.Email, &user.PasswordHash)
-	if err != nil {
-		return nil, fmt.Errorf("user:%w", err)
-	}
-
 	return &user, nil
 }
 
